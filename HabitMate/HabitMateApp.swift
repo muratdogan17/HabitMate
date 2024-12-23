464a14e641d364a0d602524b4225a97f6dfa0302
Murat Doğan <muratdogan@yahoo.com>
2024-12-23 23:41:56 +0300

    Initial Commit

---
+++ HabitMate/HabitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMateApp.swift
+//  HabitMate
+//
+//  Created by Murat Doğan on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitMateTests/HabitMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMateTests.swift
+//  HabitMateTests
+//
+//  Created by Murat Doğan on 23.12.2024.
+//
+
+import Testing
+@testable import HabitMate
+
+struct HabitMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
